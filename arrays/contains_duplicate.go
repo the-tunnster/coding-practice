48b998a7d89f11ffcb8a381d97031a89f64ebb06
the-tunnster <therealtarunkrishnan@gmail.com>
2024-02-18 22:46:58 +0000

    Added options for time and space complexity.

--- arrays/contains_duplicate.go
+++ arrays/contains_duplicate.go
@@ -1,15 +1,33 @@
 package arrays
 
-func ContainsDuplicate(input []int) bool {
+import "sort"
 
-    varMap := make(map[int]int)
+func ContainsDuplicate(input []int, complexity string) bool {
 
-    for _, v := range input{
-        if varMap[v] == 1 {
-            return true
-        }
-        varMap[v] = 1
+    switch (complexity){
+        case "time":
+            varMap := make(map[int]int)
+
+            for _, v := range input{
+                if varMap[v] == 1 {
+                    return true
+                }
+                varMap[v] = 1
+            }
+
+            return false
+
+        case "space":
+            sort.Ints(input)
+            for i:=0; i<len(input)-1; i++{
+                if input[i] == input[i+1]{
+                    return true
+                }
+            }
+
+            return false
+
     }
-
+    
     return false
 }
